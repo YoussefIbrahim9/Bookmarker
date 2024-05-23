f5868a12ed766496b2ad9ee08d7be90682ea047e
YoussefIbrahim9 <youssefhasssan873@gmail.com>
2024-05-23 14:58:43 +0200

    Edit and modal

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 var siteName = document.getElementById("siteName");
 var siteUrl = document.getElementById("siteUrl");
+
+var oldSiteName = document.getElementById("oldSiteName");
+var oldSiteUrl = document.getElementById("oldSiteUrl");
+
+var newSiteName = document.getElementById("newSiteName");
+var newSiteUrl = document.getElementById("newSiteUrl");
+
+
+
+
 var inputList = [];
 
 
@@ -13,14 +23,50 @@ else{
 }
 
 
-function updatedSite(idx){
+function editSite(){
+    var obj = {
+        oldsitename: oldSiteName.value,
+        oldsiturl: oldSiteUrl.value,
+        newsitename: newSiteName.value,
+        newsiteurl: newSiteUrl.value
+    }
     
+    for(var i = 0; i < inputList.length ;i++)
+        {
+            if(inputList[i].siteNickName == obj.oldsitename && inputList[i].siteURL == obj.oldsiturl ){
+
+                inputList[i].siteNickName = obj.newsitename;
+                inputList[i].siteURL = obj.newsiteurl;
+                break
+            }
+            else if(inputList[i].siteNickName == obj.oldsitename){
+
+                inputList[i].siteNickName = obj.newsitename;
+                break
+            }
+            else if(inputList[i].siteURL == obj.oldsiturl){
+                inputList[i].siteURL = obj.newsiteurl;
+                break
+            }
+        }
+    oldSiteName.value = null;
+    oldSiteUrl.value = null;
+    newSiteName.value = null;
+    newSiteUrl.value = null;
+
+    localStorage.setItem("siteDetails", JSON.stringify(inputList))
+    displaySite();
+
 }
 
 
 function deleteSite(indx){
     inputList.splice( indx,1);
     localStorage.setItem("siteDetails", JSON.stringify(inputList))
+    
+    if(inputList.length==0){
+        localStorage.clear();
+    }
     displaySite();
 }
 
@@ -38,8 +84,8 @@ function displaySite(){
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center">${inputList[i].siteNickName}</td>
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center "> <button class="btn btn-success "> <a target="_blank" href="${inputList[i].siteURL}" class="text-decoration-none text-white" > <i class="fa-solid fa-eye"></i> Visit</a> </button> </td>
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button onclick=" deleteSite(${i})" class="btn btn-danger">  <i class="fa-solid fa-trash"></i> Delete </button> </td>
-        <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button onclick="updatedSite(${i})" class="btn btn-warning text-white">  <i class="fa-solid fa-pen-to-square"></i> Edit </button> </td>
-    </tr>`
+        <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button id= " ${i} " class="btn btn-warning text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">  <i class="fa-solid fa-pen-to-square"></i> Edit </button> </td>
+    </tr>` 
     }
     document.getElementById("contentShow").innerHTML = content;
 }
