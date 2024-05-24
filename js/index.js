268f70e5b017055e54a09813a0442fb50aac41dd
YoussefIbrahim9 <youssefhasssan873@gmail.com>
2024-05-24 18:52:00 +0200

    validation added

--- js/index.js
+++ js/index.js
@@ -7,9 +7,6 @@ var oldSiteUrl = document.getElementById("oldSiteUrl");
 var newSiteName = document.getElementById("newSiteName");
 var newSiteUrl = document.getElementById("newSiteUrl");
 
-
-
-
 var inputList = [];
 
 
@@ -21,6 +18,26 @@ if(localStorage.length != 0){
 else{
     inputList = [];
 }
+
+
+function validation(element){
+    var regex = {
+        siteName:/^[A-Z]\w{2,}/,
+        siteUrl: /^https:/ 
+    }
+
+    element.classList.remove("is-valid");
+    element.classList.remove("is-invalid");
+
+    if(regex[element.id].test(element.value)){
+        element.classList.add("is-valid");
+    }
+    else{
+         element.classList.add("is-invalid");
+    }
+}
+
+
 
 
 function editSite(){
@@ -74,6 +91,8 @@ function deleteSite(indx){
 function clearSite(){
     siteName.value = null;
     siteUrl.value= null;
+    siteName.classList.remove("is-valid","is-invalid");
+    siteUrl.classList.remove("is-valid","is-invalid");
 }
 
 function displaySite(){
@@ -84,7 +103,7 @@ function displaySite(){
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center">${inputList[i].siteNickName}</td>
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center "> <button class="btn btn-success "> <a target="_blank" href="${inputList[i].siteURL}" class="text-decoration-none text-white" > <i class="fa-solid fa-eye"></i> Visit</a> </button> </td>
         <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button onclick=" deleteSite(${i})" class="btn btn-danger">  <i class="fa-solid fa-trash"></i> Delete </button> </td>
-        <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button id= " ${i} " class="btn btn-warning text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">  <i class="fa-solid fa-pen-to-square"></i> Edit </button> </td>
+        <td class="w-25 p-2 bg-white d-flex justify-content-center align-items-center"> <button class="btn btn-warning text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">  <i class="fa-solid fa-pen-to-square"></i> Edit </button> </td>
     </tr>` 
     }
     document.getElementById("contentShow").innerHTML = content;
@@ -92,15 +111,22 @@ function displaySite(){
 
 function addSite(){
 
-    var inputDetails = 
-    {
-        siteNickName: siteName.value,
-        siteURL : siteUrl.value
+    if(siteName.classList.contains("is-valid") && siteUrl.classList.contains("is-valid")){
+        var inputDetails = 
+        {
+            siteNickName: siteName.value,
+            siteURL : siteUrl.value
+        }
+        inputList.push(inputDetails);
+        localStorage.setItem("siteDetails", JSON.stringify(inputList))
+        
+        clearSite()
+        displaySite()
     }
-    inputList.push(inputDetails);
-    localStorage.setItem("siteDetails", JSON.stringify(inputList))
-    
-    clearSite()
-    displaySite()
+    else{
+        window.alert("Please insert a valid input");
+    }
+
+
 }
 
